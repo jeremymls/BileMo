a1c84d375f5a5c2812e00ab908d002107bcc65e1
jeremy-mls <jmonlouis@live.fr>
2023-07-15 12:09:47 +0200

    Récupérez les données

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,18 +5,20 @@ namespace App\Controller;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class ProductController extends AbstractController
 {
     /**
      * @Route("/api/products", name="app_product")
      */
-    public function getProductList(): JsonResponse
+    public function getProductList(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ProductController.php',
-        ]);
+        $productsList = $productRepository->findAll();
+        $jsonProductsList = $serializer->serialize($productsList, 'json');
+
+        return new JsonResponse($jsonProductsList, Response::HTTP_OK, [], true);
     }
 }
