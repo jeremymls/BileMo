173c356aba4b89e0b825a807083b5c2a1ec25748
jeremy-mls <jmonlouis@live.fr>
2023-07-15 12:01:08 +0200

    Créer la route /api/products

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/api/products", name="app_product")
+     */
+    public function getProductList(): JsonResponse
+    {
+        return $this->json([
+            'message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/ProductController.php',
+        ]);
+    }
+}
