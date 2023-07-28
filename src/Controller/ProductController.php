34b54a5d6dc5dc8ee69dd13b8ef36c5f5ea11480
jeremy-mls <jmonlouis@live.fr>
2023-07-28 13:11:00 +0200

    Installez HATEOAS et JMSSerializer

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,12 +5,13 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use JMS\Serializer\SerializationContext;
+use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
 
@@ -31,8 +32,9 @@ class ProductController extends AbstractController
         $idCache = 'product_list_' . $page . '_' . $limit;
         $jsonProductsList = $cachePool->get($idCache, function (ItemInterface $item) use ($productRepository, $page, $limit, $serializer) {
             $item->tag('product_list');
+            $context = SerializationContext::create()->setGroups(['getProducts']);
             $productsList = $productRepository->findAllWithPagination($page, $limit);
-            return $serializer->serialize($productsList, 'json', ['groups' => 'getProducts']);
+            return $serializer->serialize($productsList, 'json', $context);
         });
 
         return new JsonResponse($jsonProductsList, Response::HTTP_OK, [], true);
@@ -44,14 +46,15 @@ class ProductController extends AbstractController
     public function getDetailProduct(Product $product, SerializerInterface $serializer, EntityManagerInterface $em): JsonResponse
     {
         $qb = $em->createQueryBuilder();
-        $qb->select('p.ref', 'p.name', 'SUM(c.quantity) as total')
+        $qb->select('p.ref', 'p.name', 'SUM(c.quantity) as total_commands')
             ->from('App\Entity\Cart', 'c')
             ->leftJoin('c.product', 'p')
             ->where('p.ref = :ref')
             ->groupBy('p.id')
             ->setParameter('ref', $product->getRef());
         $product = $qb->getQuery()->getResult();
-        $jsonProduct = $serializer->serialize($product, 'json', ['groups' => ['getProducts', 'getProduct']]);
+        $context = SerializationContext::create()->setGroups(['getProduct, getProducts']);
+        $jsonProduct = $serializer->serialize($product, 'json', $context);
 
         return new JsonResponse($jsonProduct, Response::HTTP_OK, [], true);
     }
